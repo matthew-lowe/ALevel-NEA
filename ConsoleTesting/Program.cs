471262e246f9ac5cfd933d6f0ec2f21164790cce
Matthew Lowe <jedijasper2004@gmail.com>
2022-01-25 13:17:47 +0000

    functions boi

--- ConsoleTesting/Program.cs
+++ ConsoleTesting/Program.cs
@@ -3,29 +3,29 @@ using System.Collections.Generic;
 using MathsLibrary;
 using MathsLibrary.Token;
 
-namespace ConsoleTesting
-{
-    class Program
-    {
-        static void PrintList(List<IToken> tokens)
-        {
-            foreach (IToken t in tokens)
-            {
+namespace ConsoleTesting {
+    class Program {
+        static void PrintList(List<IToken> tokens) {
+            foreach (IToken t in tokens) {
                 Console.Write(t.ToString() + " ");
             }
+            Console.WriteLine();
         }
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             while (true) {
                 Console.Write("> ");
                 string input = Console.ReadLine();
-                var lexer = new Lexer();
-                var parser = new Parser();
-                var tokens = lexer.Tokenize(input);
-                var tree = parser.Next(tokens);
-                var result = Executor.Evaluate(tree);
-                Console.WriteLine(result);
+                try {
+                    var lexer = new Lexer();
+                    var parser = new Parser();
+                    var tokens = lexer.Tokenize(input);
+                    var tree = parser.Next(tokens);
+                    var result = Executor.Evaluate(tree);
+                    Console.WriteLine(Math.Round(result, 8));
+                } catch (Exception e) {
+                    Console.WriteLine("There may or may not have been a mild oopsie");
+                }
             }
         }
     }
